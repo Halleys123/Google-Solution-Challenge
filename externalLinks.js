55cec1f90b31f30395ac30bb748a647db52e86d0
Halleys123 <65032719+Halleys123@users.noreply.github.com>
2023-03-31 00:50:53 +0530

    temporary commit for viewing changes

--- externalLinks.js
+++ externalLinks.js
@@ -1,5 +1,10 @@
-document.querySelectorAll("cardTwoImage").forEach((image) => {
-  image.addEventListener("click", () => {
-    console.log(image);
+document.querySelectorAll(".card2").forEach((card) => {
+  card.addEventListener("click", () => {
+    window.open(card.getAttribute("data-link"), "_blank");
+  });
+});
+document.querySelectorAll(".link").forEach((entry) => {
+  entry.addEventListener("click", (e) => {
+    window.location = `${e.currentTarget.id}.html`;
   });
 });
